e1283488409e1f0081da0534e997e2b17f8992f3
Isac pacheco <isac.pachecolopez@unicolombo.edu.co>
2024-04-24 16:36:42 -0500

    Menus emergentes

---
+++ guia_13/src/mouseclicked/MouseClicked.java
@@ -0,0 +1,21 @@
+
+package mouseclicked;
+
+/**
+ *
+ * @author isac pacheco
+ */
+public class MouseClicked {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal principal = new Principal();
+        principal.setLocationRelativeTo(null);
+        principal.setVisible(true);
+       
+    }
+    
+}
